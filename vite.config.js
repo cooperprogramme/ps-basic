1d2bbdaf3de399af5fd3c316d775da88021f24a3
cooperprogramme <helloacooper@gmail.com>
2023-05-08 18:43:22 +0100

    update config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'http://webdevcorso.altervista.org/ps-basic/',
+  // base: '/ps-basic/',
   plugins: [react()],
   resolve: {
     alias: {
