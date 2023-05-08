23a2f14f59266990c94b9605fc62c8393b318274
cooperprogramme <helloacooper@gmail.com>
2023-05-08 21:32:43 +0100

    changed base to url https since it looks like that's what it needs :)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ps-basic.xyz/',
+  base: 'https://ps-basic.xyz/',
   plugins: [react()],
   resolve: {
     alias: {
